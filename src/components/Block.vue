d0739e37b9ae70740687cb391dcf766ec9e6dc31
Camilo <camilo.saa@gmail.com>
2021-04-22 15:54:34 +0200

    adding hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock">
     click me
   </div>
 </template>
@@ -7,6 +7,16 @@
 <script>
 export default {
   props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
 };
 </script>
 
@@ -14,7 +24,7 @@ export default {
 .block {
   width: 400px;
   border-radius: 20px;
-  background: #0faf87;
+  background: #1763ae;
   color: white;
   text-align: center;
   padding: 100px 0;
